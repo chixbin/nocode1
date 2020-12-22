0103006a7306d0938fda45be4dca3d422dc589e5
chixbin <56855733+chixbin@users.noreply.github.com>
2020-12-22 13:57:07 +0800

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fe188a498cea1001eae29ca",
+				"5fe188a498cea1001eae29cc"
+			]
+		},
+		"5fe188a498cea1001eae29ca": {
+			"id": "5fe188a498cea1001eae29ca",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fe188a498cea1001eae29cc": {
+			"id": "5fe188a498cea1001eae29cc",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,276 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box, Icon, Input, Button } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, Menu, Section, StackItem, Stack, Formspree } from "@quarkly/components";
+import { MdArrowDownward } from "react-icons/md";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section height="40px" display="flex">
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="space-between"
+				align-items="center"
+				flex-direction="row"
+				md-flex-direction="column"
+			>
+				<Text margin="0" md-margin="0px 0 20px 0" text-align="left" font="--lead">
+					Awesome Company
+				</Text>
+				<Menu
+					display="flex"
+					justify-content="center"
+					font="--base"
+					font-weight="700"
+					md-flex-direction="column"
+					md-align-items="center"
+				>
+					<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+					<Override slot="link-active" color="--primary" />
+					<Override slot="item" padding="6px" />
+				</Menu>
+			</Box>
+		</Section>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://images.unsplash.com/photo-1510125594188-5afc74c8cc43?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
+						Excellence in everything
+					</Text>
+					<Text as="h1" font="--headline1" md-font="--headline2" margin="10px 0">
+						Striving for perfection in everything we do. Unparalleled service for everyone.
+					</Text>
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0">
+				<Text margin="8px 0" text-transform="uppercase">
+					Who we are
+				</Text>
+				<Icon category="md" margin="0 auto" icon={MdArrowDownward} />
+			</Box>
+		</Section>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://images.unsplash.com/photo-1515600051222-a3c338ff16f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80) center/cover">
+			<Box max-width="240px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
+				<Text
+					as="h4"
+					font="--base"
+					color="--grey"
+					letter-spacing="1px"
+					text-transform="uppercase"
+					margin="6px 0"
+				>
+					About me
+				</Text>
+				<Text as="h2" font="--headline2" margin="0 0 12px 0">
+					Hey I’m David
+				</Text>
+				<Text font="--base">
+					My name is David R. Moore, born and raised in France, worked as a professional photographer and videographer for more than 10 years, awarded the CDS Documentary Essay Prize in 2018 and Best Photographer of The Year 2019 by Sony World Photography Awards.
+				</Text>
+			</Box>
+		</Section>
+		<Section padding="64px 0" sm-padding="40px 0" color="--dark">
+			<Stack>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text as="h2" font="--headline1" margin="0 0 8px 0">
+						FAQs
+					</Text>
+					<Text as="p" font="--lead" margin="0" color="--greyD2">
+						Need answers? We got’em
+					</Text>
+				</StackItem>
+				<StackItem width="66.66%" md-width="100%">
+					<Stack>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="12px 0">
+								Is there life on Mars?
+							</Text>
+							<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+								It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="12px 0">
+								Why is the sky blue?
+							</Text>
+							<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+								It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="12px 0">
+								When will we get there?
+							</Text>
+							<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+								It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" flex-direction="column" />
+							<Text as="h3" font="--headline3" margin="12px 0">
+								How much salt is in the ocean?
+							</Text>
+							<Text as="p" font="--base" margin="12px 0" color="--greyD2">
+								It all begins with an idea. Maybe you want to launch a business. Maybe you want to turn a hobby into something more. Or maybe you have a creative project to share with the world. Whatever it is, the way you tell your story online can make all the difference.
+							</Text>
+						</StackItem>
+					</Stack>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Stack>
+				<StackItem width="50%" lg-width="100%" lg-order="2">
+					<Override slot="StackItemContent" text-align="center" />
+					<Stack>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=582&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Nathan K. Joe
+							</Text>
+							<Text color="--grey" margin="0">
+								CEO
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1546672741-d327539d5f13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								David R. Lema
+							</Text>
+							<Text color="--grey" margin="0">
+								Developer
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Janet E. Morey
+							</Text>
+							<Text color="--grey" margin="0">
+								Marketing
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Claire R. Peery
+							</Text>
+							<Text color="--grey" margin="0">
+								Designer
+							</Text>
+						</StackItem>
+					</Stack>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" lg-margin-bottom="24px" flex-direction="column" />
+					<Text color="--grey" letter-spacing="1px" text-transform="uppercase" margin="0 0 10px 0">
+						Meet our team
+					</Text>
+					<Text as="h2" font="--headline2" md-font="--headline3" margin="0">
+						The smartest people work every day to provide the best service and to make our clients happy
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section background-color="--dark" text-align="center" padding="32px 0">
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--lead"
+				font-weight="700"
+				margin="-6px 0 16px"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--light" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px 0px" />
+			</Menu>
+			<Link
+				href="mailto:hello@company.com"
+				text-decoration-line="none"
+				variant="--base"
+				color="--grey"
+				hover-color="--primary"
+			>
+				chi070508@gmail.com
+			</Link>
+		</Section>
+		<Formspree errorMessage="Something went wrong" completeText="Success">
+			<Input name="email" placeholder="Email" margin-right="4px" />
+			<Button>
+				Submit
+			</Button>
+		</Formspree>
+		<Section />
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
